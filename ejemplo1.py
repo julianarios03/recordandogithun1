407f768eadf7d1439b22a67f688b169cb5d6c926
yesline03 <ayesk03@gmail.com>
2023-08-15 08:08:08 -0500

    feat:creamos variables primitivas y lista de datos

---
+++ ejemplo1.py
@@ -0,0 +1,12 @@
+#Comentario Linea
+nombreUsuario="Juliana Andrea Rios Guarin"
+edadUsuario=22
+estaturaUsuario=1.68
+esHinchaDelVerde=True
+
+print(f"Su nombre es:{nombreUsuario} y tiene {edadUsuario} años.")
+
+comidasFavoritas=["Lentejas","Pastas","Arroz","Carne Res","Salchipapas"]
+
+print(comidasFavoritas)
+print(comidasFavoritas[2])
